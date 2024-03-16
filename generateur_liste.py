c13bbf34acf5ea0944f9e3ee5c1aea128cd541e8
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-03-16 15:35:11 +0100

    Add GPS coordinates to grid cells

--- generateur_liste.py
+++ generateur_liste.py
@@ -29,6 +29,10 @@ print("longueur y_grid", len(y_grid))
 
 grille = []
 centres_lambert = []
+sommets1_lambert = []
+sommets2_lambert = []
+sommets3_lambert = []
+sommets4_lambert = []
 
 # Calculer la moyenne des points dans chaque cellule de la grille
 for i in range(len(x_grid)-1):
@@ -42,6 +46,10 @@ for i in range(len(x_grid)-1):
         sommet3_lambert = (x_upper, y_upper)
         sommet4_lambert = (x_lower, y_upper)
         centres_lambert.append(centre_lambert)
+        sommets1_lambert.append(sommet1_lambert)
+        sommets2_lambert.append(sommet2_lambert)
+        sommets3_lambert.append(sommet3_lambert)
+        sommets4_lambert.append(sommet4_lambert)
         if len(indices[0]) > 0:
             average_power = np.mean(dbms[indices])
             releves = list(zip(ids[indices], [int(e) for e in dbms[indices]], technos[indices]))
@@ -66,9 +74,17 @@ for i in range(len(x_grid)-1):
                 'type': None})            
 
 x_centres_gps, y_centres_gps = transform(lambert93, wgs84, np.array(centres_lambert)[:, 0], np.array(centres_lambert)[:,1])
+x_sommets1_gps, y_sommets1_gps = transform(lambert93, wgs84, np.array(sommets1_lambert)[:, 0], np.array(sommets1_lambert)[:,1])
+x_sommets2_gps, y_sommets2_gps = transform(lambert93, wgs84, np.array(sommets2_lambert)[:, 0], np.array(sommets2_lambert)[:,1])
+x_sommets3_gps, y_sommets3_gps = transform(lambert93, wgs84, np.array(sommets3_lambert)[:, 0], np.array(sommets3_lambert)[:,1])
+x_sommets4_gps, y_sommets4_gps = transform(lambert93, wgs84, np.array(sommets4_lambert)[:, 0], np.array(sommets4_lambert)[:,1])
 print(len(x_centres_gps))
 for i in range(len(x_centres_gps)):
     grille[i]['centre_gps'] = (float(x_centres_gps[i]), float(y_centres_gps[i]))
+    grille[i]['s1_gps'] = (float(x_sommets1_gps[i]), float(y_sommets1_gps[i]))
+    grille[i]['s2_gps'] = (float(x_sommets2_gps[i]), float(y_sommets2_gps[i]))
+    grille[i]['s3_gps'] = (float(x_sommets3_gps[i]), float(y_sommets3_gps[i]))
+    grille[i]['s4_gps'] = (float(x_sommets4_gps[i]), float(y_sommets4_gps[i]))
     
 with open("grille.pkl", "wb") as fichier:
     pickle.dump({'grille': grille,
