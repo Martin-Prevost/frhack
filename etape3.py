3dd5818203e609df03c7f3f7e3f4e240a14f81e8
Martin Prévost <martin.mprevost@gmail.com>
2024-03-16 17:20:57 +0100

    test e3

--- etape3.py
+++ etape3.py
@@ -7,8 +7,6 @@ with open('type.pkl', 'rb') as f:
     data = pickle.load(f)
     grille, len_x, len_y = data['grille'], data['len_x_grid'], data['len_y_grid']
 
-
-traiter = [[False for _ in range(len_y)] for _ in range(len_x)]
 grid = np.array(grille).reshape(len_x-1, len_y-1)
 res = []
 
@@ -29,7 +27,6 @@ def replace_with_big_square(i, j, size, value):
     dbm_count = 0
     for row in range(i, i + size):
         for col in range(j, j + size):
-            traiter[row][col] = True
             if grid[row][col]['dbm_moy'] != 0:
                 dbm_somme += grid[row][col]['dbm_moy']
                 dbm_count += len(grid[row][col]['releves'])
@@ -46,23 +43,32 @@ def replace_with_big_square(i, j, size, value):
     
 
 def process_grid():
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            if not traiter[i][j]:
-                type_1, type_2, type_3 = 'URB', 'PER', 'RUR'
-                if grid[i][j]['type'] == type_2 and detect_big_square(i, j, 2, type_2):
-                    replace_with_big_square(i, j, 2, type_2)
-                elif grid[i][j]['type'] == type_3 and detect_big_square(i, j, 3, type_3):
-                    replace_with_big_square(i, j, 3, type_3)
-                elif grid[i][j]['type'] != None:
-                    res.append({
-                        's1_gps': grid[i][j]['s1_gps'],
-                        's2_gps': grid[i][j]['s2_gps'],
-                        's3_gps': grid[i][j]['s3_gps'],
-                        's4_gps': grid[i][j]['s4_gps'],
-                        'dbm_moy': grid[i][j]['dbm_moy'],
-                        'type': grid[i][j]['type']
-                    })
+    type_1, type_2, type_3 = 'URB', 'PER', 'RUR'
+    for i in range(0, grid.shape[0], 3):
+        for j in range(0, grid.shape[1], 3):
+            nb_type_1, nb_type_2, nb_type_3 = 0, 0, 0
+            for row in range(i, i + 3):
+                for col in range(j, j + 3):
+                    # compter le nombre de type_1, type_2, type_3
+                    nb_type_1 += 1 if grid[row][col]['type'] == type_1 else 0
+            
+            nb_type_1, nb_type_2, nb_type_3 = grid[i:i+3, j:j+3]['type'].tolist().count('URB'), grid[i:i+3, j:j+3]['type'].tolist().count('PER'), grid[i:i+3, j:j+3]['type'].tolist().count('RUR')
+            print(nb_type_1, nb_type_2, nb_type_3)
+            return
+            type_1, type_2, type_3 = 'URB', 'PER', 'RUR'
+            if grid[i][j]['type'] == type_2 and detect_big_square(i, j, 2, type_2):
+                replace_with_big_square(i, j, 2, type_2)
+            elif grid[i][j]['type'] == type_3 and detect_big_square(i, j, 3, type_3):
+                replace_with_big_square(i, j, 3, type_3)
+            elif grid[i][j]['type'] != None:
+                res.append({
+                    's1_gps': grid[i][j]['s1_gps'],
+                    's2_gps': grid[i][j]['s2_gps'],
+                    's3_gps': grid[i][j]['s3_gps'],
+                    's4_gps': grid[i][j]['s4_gps'],
+                    'dbm_moy': grid[i][j]['dbm_moy'],
+                    'type': grid[i][j]['type']
+                })
 
 process_grid()
 print(len(res))
