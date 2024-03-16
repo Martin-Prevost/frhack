f3fc720825f19a19758d3ff90e3dc6fa73e2198f
Theophile-Wemaere <theophile.wemaere@gmail.com>
2024-03-16 14:53:45 +0100

    fixed type error in test_shapefile

--- test_shapefile.py
+++ test_shapefile.py
@@ -5,14 +5,28 @@ import shapely.geometry as sg
 import matplotlib.pyplot as plt
 import sys
 
+files = [
+    "data/Shape Depts 41 45 89",
+    "data/Shape Blois Orleans Auxerre"
+    "data/Zones RURALES 41 45 89",
+    "data/Zones URBAINES 41 45 89",
+    "data/Zones PERI URBAINES 41 45 89"
+]
+
+#for file in files: 
+#    print(file)
 r = shapefile.Reader(sys.argv[1])
 
 shape_obj = r.shapeRecords()[0]
 geom = shape_obj.shape.__geo_interface__
 for coord in geom["coordinates"]:
-    datas = sg.Polygon(coord[0])
+    datas = None
+    if not isinstance(coord[0],list):
+        datas = sg.Polygon(coord)
+    else:
+        datas = sg.Polygon(coord[0])
     x, y = datas.exterior.xy
-
     plt.plot(x, y)
+
 plt.show()
 
